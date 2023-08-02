5ec37bbd5aa1227ecd5ac5356071c3f7ce3e4276
Andre2828Ferreira88 <123772724+Andre2828Ferreira88@users.noreply.github.com>
2023-08-02 11:33:41 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,8 @@
+//Numero Kelvin = 293
+let kelvin = 293
+let celsius = 273
+let fahrenheit = Math.floor (celsius * (9/5)+ 32.)
+console.log(" a temperatura é " + fahrenheit + " graus Fahrenheit")
+
+let fahrenhei = Math.floor (kelvin * (9/5)+ 32.)
+console.log(" a temperatura é " + fahrenhei + " de kelvin em Fahrenheit")
